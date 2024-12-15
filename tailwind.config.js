d6936a7324a1065954219caa2b21d1312332dccc
Amit Naskar <amitnaskar2003@gmail.com>
2024-12-15 18:34:39 +0530

    Tailwind config file modified

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,7 @@
 //   plugins: [],
 // }
 
-module.exports = {
+export default {
   darkMode: 'class', // Enable class-based dark mode
   content: ['./index.html', './src/**/*.{js,jsx}'],
   theme: {
